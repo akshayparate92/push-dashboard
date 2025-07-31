16332793d60403c1568d764f98869b01d8283fef
akshayparate92 <akshayparate92@gmail.com>
2025-07-31 13:07:26 +0530

    rebase

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/login');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200); // working stash check
     }
 }
